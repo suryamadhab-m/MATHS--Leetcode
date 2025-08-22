acf6762ce32543ccd8a4e307294b52608587d618
Suryamadhab Moharana <147189262+suryamadhab-m@users.noreply.github.com>
2025-08-22 16:34:06 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (12.96%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def tribonacci(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n==0:
+            return 0
+        elif n==1:
+            return 1
+        elif n==2:
+            return 1
+        else:
+            a=0
+            b=1
+            c=1
+            for i in range(3, n+1):
+                d=a+b+c
+                a=b
+                b=c
+                c=d
+            return c
+        
